72e15b272f63190bf18d040de758b623c8c03404
Jose Acosta <55077332+jchaidez23@users.noreply.github.com>
2023-08-07 02:35:14 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+app.use(cors());
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+
+const json_file = './data.json';
+app.get('/archive', (req,res) => {
+    res.json(require(json_file));
+});
+app.get('/',(req,res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+module.exports = app;
+
+const http = require('http');
+
+const port = 3000;
+
+const server = app.listen(3000, () => {
+    console.log(`Server is connected at port: ${port}`);
+})
+
+
